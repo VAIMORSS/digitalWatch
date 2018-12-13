b6a6d3afd2afea5dd51dffec261840b0cbb02888
Vaibhav <31856944+VAIMORSS@users.noreply.github.com>
2018-12-12 20:04:51 -0500

    Update ClockView.java

--- java/com/map524s1a/digitalclock/ClockView.java
+++ java/com/map524s1a/digitalclock/ClockView.java
@@ -1,5 +1,6 @@
 package com.map524s1a.digitalclock;
 
+
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -9,18 +10,26 @@ import android.view.View;
 import java.util.Calendar;
 import java.util.Date;
 
+import static android.graphics.Color.rgb;
+
 
 public class ClockView extends View {
 
     int date, month, year, hour, minute, second;
+    String amOrPm="AM";
+
     Paint minutePaint = new Paint();
     Paint hourPaint = new Paint();
     Paint secondPaint = new Paint();
+    Paint digitalClockPaint = new Paint();
+    Paint fullRing = new Paint();
+
+
 
     Paint text = new Paint();
 
     int centarPoint=750;
-    int radiusForSecond=450, radiusForMinute=350,radiusForHour=250,textRadius=550;
+    int radiusForSecond=500, radiusForMinute=400,radiusForHour=300,textRadius=600, digitalClockPlace=180, fullRingRad=550;
     Calendar currentTime;
 
 
@@ -28,6 +37,7 @@ public class ClockView extends View {
     RectF hourRectF;
     RectF minuteRectF;
     RectF secondRectF;
+    RectF fullRingF;
 
     public ClockView(Context context){
         super(context);
@@ -38,9 +48,9 @@ public class ClockView extends View {
         this.minute=0;
         this.second=0;
 
-        hourPaint.setColor(Color.MAGENTA);
         hourPaint.setStyle(Paint.Style.STROKE);
         hourPaint.setStrokeWidth(20);
+        hourPaint.setColor(Color.MAGENTA);
 
         minutePaint.setColor(Color.GREEN);
         minutePaint.setStyle(Paint.Style.STROKE);
@@ -50,15 +60,23 @@ public class ClockView extends View {
         secondPaint.setStyle(Paint.Style.STROKE);
         secondPaint.setStrokeWidth(20);
 
+        fullRing.setColor(Color.GRAY);
+        fullRing.setStyle(Paint.Style.STROKE);
+        fullRing.setStrokeWidth(10);
+
         text.setColor(Color.BLACK);
         text.setTextSize(150);
 
+        digitalClockPaint.setTextSize(65);
 
 
 
         hourRectF = new RectF(centarPoint-radiusForHour, centarPoint-radiusForHour, centarPoint+radiusForHour, centarPoint+radiusForHour);
         minuteRectF = new RectF(centarPoint-radiusForMinute, centarPoint-radiusForMinute, centarPoint+radiusForMinute, centarPoint+radiusForMinute);
         secondRectF =  new RectF(centarPoint-radiusForSecond, centarPoint-radiusForSecond, centarPoint+radiusForSecond, centarPoint+radiusForSecond);
+        fullRingF =  new RectF(centarPoint-fullRingRad, centarPoint-fullRingRad, centarPoint+fullRingRad, centarPoint+fullRingRad);
+
+
     }
 
     public void timeSetter(){
@@ -66,10 +84,15 @@ public class ClockView extends View {
         currentTime=Calendar.getInstance();
         minute=currentTime.get(Calendar.MINUTE);
         hour=currentTime.get(Calendar.HOUR);
-        if(hour>12){
-            hour-=12;
-        }
         second=currentTime.get(Calendar.SECOND);
+        month=currentTime.get(Calendar.MONTH);
+        date=currentTime.get(Calendar.DATE);
+        year=currentTime.get(Calendar.YEAR);
+        if(currentTime.get(Calendar.AM_PM)==Calendar.PM){
+            amOrPm="PM";
+        }
+        digitalClockPaint.setColor(rgb(128,(second*4)%128,0+((second*3)%128)));
+
 
     }
 
@@ -81,12 +104,21 @@ public class ClockView extends View {
         canvas.drawArc (hourRectF, 270, (hour+(minute/60))*30, false, hourPaint);
         canvas.drawArc (minuteRectF, 270, (minute+(second/60))*6, false, minutePaint);
         canvas.drawArc (secondRectF, 270, second*6, false, secondPaint);
+        canvas.drawArc (fullRingF, 0, 360, false, fullRing);
+
 
         canvas.drawText("12", centarPoint-70, textRadius-radiusForSecond+70, text);
         canvas.drawText("3", centarPoint+textRadius-40, centarPoint+70, text);
         canvas.drawText("9", centarPoint-textRadius-40, centarPoint+70, text);
         canvas.drawText("6", centarPoint, textRadius+centarPoint+70, text);
 
+        canvas.drawText( String.format("%02d",hour)+" : "+String.format("%02d",minute)+" : "+String.format("%02d",second)+" "+amOrPm,centarPoint-digitalClockPlace,centarPoint,digitalClockPaint);
+        canvas.drawText( "DATE : "+String.format("%02d",date) +"/"+String.format("%02d",month)+"/"+year,centarPoint-digitalClockPlace-70,centarPoint+800,digitalClockPaint);
+
+
+
+
+        //canvas.DrawLine(cx, cy, x, y, paint);
 
         //canvas.drawText(String.valueOf(hour),100,100,color);
         //canvas.drawText(String.valueOf(minute),100,300,color);
